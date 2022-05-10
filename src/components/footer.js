1cf0fbcb280227c255a2447e6d4b72fb03b424c1
Sugeng Tigefa <sugeng@tigefa.pw>
2022-05-10 23:21:51 +0700

    my pref

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,6 @@ const Footer = () => {
     <div id="footer">
       <div className="container">
         <p>based on <Link to="https://uptimerobot.com/" text="UptimeRobot" /> Interface production, Detection frequency 5 minutes</p>
-        <p>&copy; 2022 <Link to="https://sugeng.win/" text="SUGENG.win" /></p>
       </div>
     </div>
   );
